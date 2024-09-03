1b9231511ea105b458141b85707a7f050c290aaa
MAX1MDEV <58599802+MAX1MDEV@users.noreply.github.com>
2024-09-04 01:43:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-#"C:\Program Files (x86)\Steam\steamapps\common\Dollar\Dollar.exe"
-#"C:\Program Files (x86)\Steam\steamapps\common\Idle Kitties Online\IdleKittiesOnline.exe"
 import os
 import time
 import schedule
 import threading
 
 def run_dollar_program():
-    os.startfile(r"C:\Program Files (x86)\Steam\steamapps\common\Dollar\Dollar.exe")
-    time.sleep(20) #тут остаемся на 20 секунд
+    os.startfile("steam://rungameid/3069470")
+    time.sleep(20)  # ждем 20 секунд
     os.system("taskkill /im Dollar.exe")
 
 def run_idle_kitties_program():
-    os.startfile(r"C:\Program Files (x86)\Steam\steamapps\common\Idle Kitties Online\IdleKittiesOnline.exe")
-    time.sleep(600) #остаемся на 10 минут
+    os.startfile("steam://launch/3062030")
+    time.sleep(600)  # ждем 10 минут
     os.system("taskkill /im IdleKittiesOnline-Win64-Shipping.exe")
 
 def job_dollar():
@@ -22,14 +20,13 @@ def job_dollar():
 def job_idle_kitties():
     thread = threading.Thread(target=run_idle_kitties_program)
     thread.start()
-
-#отправляем сигнал сразу при запуске скрипта
+    
+# отправляем сигнал сразу при запуске скрипта
 job_dollar()
 job_idle_kitties()
 
-schedule.every(30).minutes.do(job_dollar) #запускаем игру каждые 30 минут
-schedule.every(3).hours.do(job_idle_kitties) #а эту запускаем каждыые 3 часа
-
+schedule.every(30).minutes.do(job_dollar)  # запускаем игру каждые 30 минут
+schedule.every(3).hours.do(job_idle_kitties)  # а эту запускаем каждые 3 часа
 def run_schedule():
     while True:
         try:
@@ -37,7 +34,7 @@ def run_schedule():
             time.sleep(1)
         except KeyboardInterrupt:
             print("Программа завершена.")
-            return
+            os._exit(0)
 
-#запускаем все в одном потоке
+# запускаем все в одном потоке
 run_schedule()
